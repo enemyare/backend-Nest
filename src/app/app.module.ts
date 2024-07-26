073b7f8a31b187346f86a408397f18aaaad92a8d
enemyare <mishasultanov4@gmail.com>
2024-07-27 03:07:43 +0500

    config + docker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '../configuration';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ConfigModule.forRoot({
+    isGlobal: true,
+    load:[configuration] 
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
